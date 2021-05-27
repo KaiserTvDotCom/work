bdcc057f87b2dc2fdde664f04a5308a80dd0070f
KaiserTvDotCom <kikioloz@hotmail.com!>
2021-05-27 17:18:44 -0500

    Mejore el split raw de ruptela para mostrar el voltaje y estado de carga del panel solar

--- Split_raw.py
+++ Split_raw.py
@@ -117,7 +117,7 @@ eventos_2B="0x" + eventos_2B
 eventos_2B=int(eventos_2B,0)
 print("Numero de eventos de 2 bytes: " + str(eventos_2B))
 
-def ObtenerEventos2Byte():
+def ObtenerEventos2Byte(flag):
     
     I_Inicial=index+2
     for evento in range(1,eventos_2B+1):
@@ -130,13 +130,37 @@ def ObtenerEventos2Byte():
         evento_2B_Value="0x" + evento_2B_Value
         evento_2B_Value=int(evento_2B_Value,0)
 
-        print("ID= " + str(evento_2B_ID),"Valor: " + str(evento_2B_Value))
+        if evento_2B_ID==22:
+            v_fotocelda=int(evento_2B_Value)
+        if evento_2B_ID==29:
+            v_bateria=int(evento_2B_Value)
+        if flag==0: 
+            pass
+        else:
+            if evento_2B_ID==22:
+                pass
+            else:
+                print("ID= " + str(evento_2B_ID),"Valor: " + str(evento_2B_Value))
         
         I_Inicial+=8
-    return   I_Inicial
+    return   I_Inicial,v_bateria,v_fotocelda
 
-index=ObtenerEventos2Byte()
+v_fotocelda=ObtenerEventos2Byte(0)[2]
+v_bateria=ObtenerEventos2Byte(0)[1]
+index=ObtenerEventos2Byte(1)[0]
 
+if v_fotocelda==0:
+    v_panel=v_bateria
+    label= "Cargando..."
+elif v_fotocelda > 35 and v_fotocelda < 100:
+    v_panel=0
+    label= "Panel Desconectado!"
+else:
+    v_panel=v_bateria - v_fotocelda
+    label= "No hay suficiente luz para cargar."
+
+print("ID= 22 Valor:",v_panel)
+print("Estado de carga:",label)
 
 #Numero de eventos que tienen 4 bytes
 eventos_4B=string[index:index+2]
--- coursera.py
+++ coursera.py
@@ -1,6 +1,5 @@
 import csv
-
-f = open("csv_file.txt")
-csv_f= csv.reader(f)
-for row in csv_f:
-    
+hosts=[["Workstation.local","192.168.25.46"],["webserver.cloud","10.142.10.61"]]
+with open ("hosts.csv", "w") as hosts_csv:
+    writer=csv.writer(hosts_csv)
+    writer.writerows(hosts)
