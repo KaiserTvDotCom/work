343e68cfa24111a2a08f89f28dac43b246e8e7ca
KaiserTvDotCom <kikioloz@hotmail.com!>
2021-05-31 17:16:43 -0500

    Agrege el archivo de medidor de voltajes para los reportes de bateria de los solares

--- coursera.py
+++
@@ -1,5 +0,0 @@
-import csv
-hosts=[["Workstation.local","192.168.25.46"],["webserver.cloud","10.142.10.61"]]
-with open ("hosts.csv", "w") as hosts_csv:
-    writer=csv.writer(hosts_csv)
-    writer.writerows(hosts)
